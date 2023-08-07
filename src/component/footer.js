f093a61ec71a1f6bca360d76d0e3460579e0f50c
Winfr1th <81304479+Winfr1th@users.noreply.github.com>
2023-08-07 12:28:31 +0800

    Fix responsiveness

--- src/component/footer.js
+++ src/component/footer.js
@@ -9,29 +9,42 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import textLogo from "../asset/jfest-text-logo.png";
 import imgLogo from "../asset/Logo JFest Large.png";
-import spiders from "../asset/Spiders.png";
-import background from "../asset/footer.png";
-import "../styles/footer.css";
+import background from "../asset/footer-bg.png";
+import footermbg from "../asset/footer-mobile-bg.png";
+import leftWeb from "../asset/footer-left-web.png";
+import rightWeb from "../asset/footer-right-web.png";
+import "./footer.css";
 
 const Footer = () => {
   const [isMobile, setIsMobile] = useState(false);
 
+  const updateisMobile = () => {
+    setIsMobile(window.innerWidth <= 768);
+  };
+
   useEffect(() => {
-    const maxWidth = window.innerWidth;
-    if (maxWidth <= 769) {
-      setIsMobile(true);
-    } else {
-      setIsMobile(false);
-    }
+    updateisMobile();
+    window.addEventListener("resize", updateisMobile);
+
+    return () => {
+      window.removeEventListener("resize", updateisMobile);
+    };
   }, []);
+
+  const imageSource = isMobile ? footermbg : background;
+
   return (
     <>
-      <div className="container-footer">
+      <div className="container">
         {isMobile ? (
-          <img className="background" src={background} alt="Backdrop" />
+          <img className="background" src={imageSource} alt="Backdrop" />
         ) : (
-          <img className="background" src={background} alt="Backdrop" />
+          <img className="background" src={imageSource} alt="Backdrop" />
         )}
+        <div className="web-container">
+          <img src={leftWeb} alt=""></img>
+          <img src={rightWeb} alt=""></img>
+        </div>
         <footer className="footer-container">
           {/* <img className="foot-spider" src={spiders} alt="JFEST 7"></img> */}
           <div className="left-footer">
